9387558c635bbe96f3bf8d0a3341e3b984dfa768
Maxim Zimakov <maximum_zima@mail.ru>
2022-08-12 22:00:48 +0300

    2

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,78 @@
+import datetime as dt
+
+import webcolors
 from rest_framework import serializers
 
-from .models import Cat
+from .models import CHOICES, Achievement, AchievementCat, Cat, Owner
+
+
+class Hex2NameColor(serializers.Field):
+
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # если в модели изменится __str__(), то изменится поле owner
+    # (т.к. StringRelatedField) в ответах API.
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+#    color = Hex2NameColor()
+    color = serializers.ChoiceField(choices=CHOICES)
+    # достаточно здесь или в модели параллельно надо тоже choises?
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        # хорошо не перегружать этот метод тяжёлыми операциями.
+        return dt.datetime.now().year - obj.birth_year
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+        # что за конструкция, где status?
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat)
+        return cat
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+    # если в модели изменится __str__(), то изменится поле cats
+    # (т.к. StringRelatedField) в ответах API.
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,39 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .models import Cat
-from .serializers import CatSerializer
+from .models import Cat, Owner
+from .serializers import CatListSerializer, CatSerializer, OwnerSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':  # а где там list появляется?!
+            # в каком-то словаре?
+            return CatListSerializer
+        return CatSerializer
+
+    @action(detail=False, url_path='recent-white-cats')  \
+        # как тут поменять поля!
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
